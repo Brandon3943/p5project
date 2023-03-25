7581bfb04b00a0958215e88686c84b5b0c2386cd
root <root@Ty-Laptop.localdomain>
2023-03-25 15:54:44 -0400

    add test route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+
+    def index
+        render json: {hello: "world"}, status: :ok
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+ 
+  get "/test", to: "application#index"
+  # root "birds#index"
+
 
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
-  
 end
