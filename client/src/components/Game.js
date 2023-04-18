83ff6c1e5b483d00342b300f30bd57638352770a
root <root@Ty-Laptop.localdomain>
2023-04-18 18:40:53 -0400

    MVP done cards to hand in game done

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -3,6 +3,12 @@ import { useState, useEffect, useRef } from 'react';
 
 function Game() {
   const [playerDeck, setPlayerDeck] = useState([])
+  const [playerHand1, setPlayerHand1] = useState(false)
+  const [playerHand2, setPlayerHand2] = useState(false)
+  const [playerHand3, setPlayerHand3] = useState(false)
+  const [playerHand4, setPlayerHand4] = useState(false)
+  const [playerHand5, setPlayerHand5] = useState(false)
+  const [playerHand6, setPlayerHand6] = useState(false)
   const canvasRef = useRef()
   let playerDeckLocation = useRef()
   let playerGraveYardLocation = useRef()
@@ -28,7 +34,7 @@ function Game() {
     }, [])
 
     useEffect(() => {
-      fetch("http://localhost:3000/current_deck")
+      fetch("/current_deck")
       .then(r => r.json())
       .then(data => setPlayerDeck(data))
       .catch(err => console.log(err))
@@ -53,10 +59,37 @@ function Game() {
       let playerTrapMonsterLocation5 = context.strokeRect(220,68,20,25)
     }
 
+    //this click removes cards from the deck and add them to the player hand
+    function handleClick() {
+      let index = Math.floor(Math.random() * playerDeck.length)      
 
-    function handleClick(value) {
-      console.log(value)
+      if(!playerHand1) {
+          setPlayerHand1(playerDeck[index])
+          setPlayerDeck(playerDeck.filter(card => playerDeck[index].id !== card.id))
+      } else if (!playerHand2) {
+          setPlayerHand2(playerDeck[index])
+          setPlayerDeck(playerDeck.filter(card => playerDeck[index].id !== card.id))
+      } else if (!playerHand3) {
+          setPlayerHand3(playerDeck[index])
+          setPlayerDeck(playerDeck.filter(card => playerDeck[index].id !== card.id))
+      } else if (!playerHand4) {
+          setPlayerHand4(playerDeck[index])
+          setPlayerDeck(playerDeck.filter(card => playerDeck[index].id !== card.id))
+      } else if (!playerHand5) {
+          setPlayerHand5(playerDeck[index])
+          setPlayerDeck(playerDeck.filter(card => playerDeck[index].id !== card.id))
+      } else if (!playerHand6) {
+          setPlayerHand6(playerDeck[index])
+          setPlayerDeck(playerDeck.filter(card => playerDeck[index].id !== card.id))
+      }  
     }
+    
+
+    console.log(playerHand1)
+
+    
+
+    
 
     
     
@@ -66,7 +99,7 @@ function Game() {
     <div>
       <canvas ref={canvasRef} />
       <div className="player_parent_container">
-        <div ref={playerDeckLocation} className="player_deck_location" onClick={() => handleClick("deck")}></div>
+        <div ref={playerDeckLocation} className="player_deck_location" onClick={() => handleClick()}></div>
         <div ref={playerGraveYardLocation} className="player_graveyard_location" onClick={() => handleClick("graveyard")}></div>
         <div ref={playerTrapMagicCardLocation1} className="player_trap_magic_location1" onClick={() => handleClick("TM1")}></div>
         <div ref={playerTrapMagicCardLocation2} className="player_trap_magic_location2" onClick={() => handleClick("TM2")}></div>
@@ -78,12 +111,24 @@ function Game() {
         <div ref={playerMonsterLocation3} className="player_monster_location3" onClick={() => handleClick("Monster3")}></div>
         <div ref={playerMonsterLocation4} className="player_monster_location4" onClick={() => handleClick("Monster4")}></div>
         <div ref={playerMonsterLocation5} className="player_monster_location5" onClick={() => handleClick("Monster5")}></div>
-        <div ref={playerHandLocation1} className="player_hand_location1" onClick={() => handleClick("Hand1")}></div>
-        <div ref={playerHandLocation2} className="player_hand_location2" onClick={() => handleClick("Hand2")}></div>
-        <div ref={playerHandLocation3} className="player_hand_location3" onClick={() => handleClick("Hand3")}></div>
-        <div ref={playerHandLocation4} className="player_hand_location4" onClick={() => handleClick("Hand4")}></div>
-        <div ref={playerHandLocation5} className="player_hand_location5" onClick={() => handleClick("Hand5")}></div>
-        <div ref={playerHandLocation6} className="player_hand_location6" onClick={() => handleClick("Hand6")}></div>
+        <div ref={playerHandLocation1} className="player_hand_location1" onClick={handleClick}>
+          {playerHand1 ? <img src={playerHand1.image_url} alt="card" style={{width: "95px"}} /> : null}
+        </div>
+        <div ref={playerHandLocation2} className="player_hand_location2" onClick={handleClick}>
+          {playerHand2 ? <img src={playerHand2.image_url} alt="card" style={{width: "95px"}} /> : null}
+        </div>
+        <div ref={playerHandLocation3} className="player_hand_location3" onClick={handleClick}>
+          {playerHand3 ? <img src={playerHand3.image_url} alt="card" style={{width: "95px"}} /> : null}
+        </div>
+        <div ref={playerHandLocation4} className="player_hand_location4" onClick={handleClick}>
+          {playerHand4 ? <img src={playerHand4.image_url} alt="card" style={{width: "95px"}} /> : null}
+        </div>
+        <div ref={playerHandLocation5} className="player_hand_location5" onClick={handleClick}>
+          {playerHand5 ? <img src={playerHand5.image_url} alt="card" style={{width: "95px"}} /> : null}
+        </div>
+        <div ref={playerHandLocation6} className="player_hand_location6" onClick={handleClick}>
+          {playerHand6 ? <img src={playerHand6.image_url} alt="card" style={{width: "95px"}} /> : null}
+        </div>
       </div>
       {/* <img src="../src/assets/YamiYugi-DULI.webp" alt="Yami Yugi" /> */}
     </div>
